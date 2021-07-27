39dc8f74c5c5062b9731225cf1f6e38aec6f6a3b
mathis <m2505e99>
2021-07-27 15:02:22 +0200

    fix cors once again

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: true }));
+app.use(cors());
 
 let transporterReady = false;
 let transporter = nodemailer.createTransport({
